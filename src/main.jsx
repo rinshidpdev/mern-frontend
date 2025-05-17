712a902f0687cd3f5a6299f5482b09165a531dac
rinu1326 <nifu4040@gmail.com>
2025-05-17 10:29:59 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { SearchProvider } from "./context/SearchContext";
+
+// createRoot(document.getElementById('root')).render(
+//   <SearchProvider>
+//     <App />
+//   </SearchProvider>
+// )
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 import { SearchProvider } from "./context/SearchContext";
 
-createRoot(document.getElementById('root')).render(
-  <SearchProvider>
-    <App />
-  </SearchProvider>
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <SearchProvider>
+        <App />
+      </SearchProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
